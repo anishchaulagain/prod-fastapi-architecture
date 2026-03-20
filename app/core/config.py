2f550a68dec2854bf6cc609fc1aded980feb275d
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-03-20 21:58:46 +0545

    add lru cache to avoid hitting config everytime

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from functools import lru_cache
 
 class Settings(BaseSettings):
     APP_NAME: str = "AI Brand Fedility",
@@ -24,4 +25,17 @@ class Settings(BaseSettings):
             f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}"
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+@lru_cache()
+def get_settings() ->str:
+    return Settings()
+
+
+settings = get_settings()
+
 
