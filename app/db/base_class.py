de6ae107d34ad219a809ad585aa32a52c268bf77
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-03-21 21:17:39 +0545

    change method for base class

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -4,10 +4,8 @@ from sqlalchemy.orm import DeclarativeBase, declared_attr
 class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        # User   → "users"
-        # Brand  → "brands"
-        # Automatically lowercases class name and adds "s"
-        return cls.__name__.lower() + "s"
+        name = re.sub(r'(?<!^)(?=[A-Z])', '_', cls.__name__).lower()
+        return name + "s"
 
     
 
